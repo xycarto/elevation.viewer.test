4250f28fba90f324600af30b9cf90e2ed311276b
Ireese <ireese@linz.govt.nz>
2020-10-16 19:25:00 +1300

    update JS

--- js/main.js
+++ js/main.js
@@ -1,70 +1,96 @@
 
 
-var settings = {
-    maxZoom: 12,
-    tms: true,
-    continuousWorld: true,
-    subdomains: '1234',
-    attribution: '<a href="https://www.linz.govt.nz/linz-copyright" target="_blank">Data sourced from LINZ. CC-BY 4.0</a>',
-    zIndex: 2
-};
-
-var aerialelev = L.tileLayer('http://maps.linz.io/tiles/wellyvation-basemap/NZTM/{z}/{x}/{y}.png', settings),
-    dem = L.tileLayer('http://maps.linz.io/tiles/wellyvation-dem-basemap/NZTM/{z}/{x}/{y}.png', settings),
-    dsm = L.tileLayer('http://maps.linz.io/tiles/wellyvation-dsm-basemap/NZTM/{z}/{x}/{y}.png', settings),
-    lowlands = L.tileLayer('https://maps.linz.io/tiles/wellyvation-lowlands-basemap/NZTM/{z}/{x}/{y}.png', settings);
-
-var baseMaps = {
-    "AerialElev": aerialelev
-};
-
-var overlayMaps = {
-    "</span><span>Aerial and Elevation</span>": aerialelev,
-    "</span><span>Digital ELEVATION Model</span>": dem,
-    "</span><span>Digital SURFACE Model</span><hr>": dsm,
-    "<span>Lowlands</span>": lowlands,
-};
-
 var crs = new L.Proj.CRS(
     'EPSG:2193',
-    '+proj=tmerc +lat_0=0 +lon_0=173 +k=0.9996 +x_0=1600000 +y_0=10000000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs', {
-        origin: [-1000000, 10000000],
-        resolutions: [
-            8960,
-            4480,
-            2240,
-            1120,
-            560,
-            280,
-            140,
-            70,
-            28,
-            14,
-            7, 
-            2.8, 
-            1.4, 
-            0.7
-        ]
+    '+proj=tmerc +lat_0=0 +lon_0=173 +k=0.9996 +x_0=1600000 +y_0=10000000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs',
+    {
+      origin: [-1000000, 10000000],
+      resolutions: [8960, 4480, 2240, 1120, 560, 280, 140, 70, 28, 14, 7, 2.8, 1.4, 0.7, 0.28, 0.14, 0.07]
     }
-);
+  );
+  
+  
+  var dem_urlTemplate = 'http://maps.linz.io/tiles/wellyvation-dem-basemap/NZTM/{z}/{x}/{y}.png'
+  
+  var dsm_urlTemplate = 'http://maps.linz.io/tiles/wellyvation-dsm-basemap/NZTM/{z}/{x}/{y}.png'
+  
+  var lowland_urlTemplate = 'https://maps.linz.io/tiles/wellyvation-lowlands-basemap/NZTM/{z}/{x}/{y}.png'
+  
+  var aerialElev_urlTemplate = 'http://maps.linz.io/tiles/wellyvation-basemap/NZTM/{z}/{x}/{y}.png'
+  
+  
+  var settings = {
+      tms: true,
+      maxZoom: 14,
+      continuousWorld: true,
+      attribution: '<a href="http://www.linz.govt.nz">Sourced from LINZ. CC-BY 4.0</a>', //Simple attribution for linz
+  };
+  
+  
+  var map = new L.Map('map', {
+      crs: crs,
+      continuousWorld: true,
+      worldCopyJump: false,
+          zoomControl: false,
+  });
+  
+  
+  var demMap = new L.TileLayer(dem_urlTemplate, settings);
+  
+  var dsmMap = new L.TileLayer(dsm_urlTemplate, settings);
+  
+  var aerialElev = new L.TileLayer(aerialElev_urlTemplate, settings);
+  
+  var lowlandMap = new L.TileLayer(lowland_urlTemplate, settings);
+  
+  var basemaps = [aerialElev, demMap, dsmMap];
+  
+  var baseMaps = {
+      "AerialElev": aerialElev
+  };
+  
+  function addmaps(basemaps) {
+          map.removeLayer(basemaps);
+          map.addLayer(basemaps);
+  };
+  
+  window.toggle = false;
+  function togglell() {
+      if(!toggle) {
+          map.addLayer(lowlandMap);
+      } else {
+          map.removeLayer(lowlandMap);
+      }
+      toggle = !toggle
+  };
+  
+  var overlayMaps = {
+      "</span><span>Aerial and Elevation</span>": aerialElev,
+      "</span><span>Digital ELEVATION Model</span>": demMap,
+      "</span><span>Digital SURFACE Model</span><hr>": dsmMap,
+      "<span>Lowlands</span>": lowlandMap
+  }
+  
+  
+  var zoomcontrol = new L.Control.Zoom({ position: 'bottomright' }).addTo(map)
+  
+  var layers = L.control.layers(baseMaps, overlayMaps, { "hideSingleBase": true }).addTo(map)
+  
+  //L.control.layers(baseMaps, overlayMaps).addTo(map);
+  map.addLayer(aerialElev);
+  
+  map.setView([-41.29, 175.4], 7);
+  
+  map.addControl(new L.Control.Permalink({ text: 'Permalink', layers: layers }));
+  
 
-var map = new L.Map('map', {
-    crs: crs,
-    continuousWorld: true,
-    worldCopyJump: false,
-    zoomControl: false,
-    maxZoom: 12,
-    minZoom: 6,
-    layers: [aerialelev]
-});
 
-var zoomcontrol = new L.Control.Zoom({ position: 'bottomright' }).addTo(map)
 
-var layers = L.control.layers(baseMaps, overlayMaps, { "hideSingleBase": true }).addTo(map);
 
-map.setView([-41.3, 175.05], 7);
 
-map.addControl(new L.Control.Permalink({ text: 'Permalink', layers: layers }));
+
+
+
 
 //setup sidebar
 var sidebar = L.control.sidebar('sidebar', {
