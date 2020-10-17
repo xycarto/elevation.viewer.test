d798eb7b285d7ed61bddf58db4ab3b912c5acec1
Ireese <ireese@linz.govt.nz>
2020-10-17 16:58:57 +1300

    switch main map

--- js/main.js
+++ js/main.js
@@ -110,7 +110,7 @@ var crs = new L.Proj.CRS(
   
   var layers = L.control.layers(basemaps, overlays).addTo(map)
   
-  map.addLayer(linzColour);
+  map.addLayer(linzAerial);
   
   map.setView([-41.29, 174.0], 3);
   
